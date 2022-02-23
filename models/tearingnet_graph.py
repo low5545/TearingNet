993e8ac544c8b739b585498b4638bb2487dcee0a
Low Weng Fei <lowwengfei@hotmail.com>
2022-02-23 20:34:09 +0800

    Remove graph filter in `GraphFilter`

--- models/tearingnet_graph.py
+++ models/tearingnet_graph.py
@@ -24,9 +24,9 @@ class GraphFilter(nn.Module):
         
         # Data preparation
         bs_cur = pc.shape[0]
-        grid_exp = grid.contiguous().view(bs_cur, self.grid_dims[0], self.grid_dims[1], 2) # batch_size X dim0 X dim1 X 2
-        pc_exp = pc.contiguous().view(bs_cur, self.grid_dims[0], self.grid_dims[1], 3) # batch_size X dim0 X dim1 X 3
-        graph_feature = torch.cat((grid_exp, pc_exp), dim=3).permute([0, 3, 1, 2])
+        grid_exp = grid.contiguous().view(bs_cur, self.grid_dims[0], self.grid_dims[1], 2)  # batch_size X dim0 X dim1 X 2
+        pc_exp = pc.contiguous().view(bs_cur, self.grid_dims[0], self.grid_dims[1], 3)      # batch_size X dim0 X dim1 X 3
+        graph_feature = torch.cat((grid_exp, pc_exp), dim=3).permute([0, 3, 1, 2])          # batch_size X 5 x dim0 X dim1
 
         # Compute the graph weights
         wght_hori = graph_feature[:,:,:-1,:] - graph_feature[:,:,1:,:] # horizontal weights
@@ -41,6 +41,8 @@ class GraphFilter(nn.Module):
         wght_bot = torch.cat((wght_vert, torch.zeros([bs_cur, self.grid_dims[0], 1]).cuda()), 2) # add bottom
         wght_all = torch.cat((wght_lft.unsqueeze(1), wght_rgh.unsqueeze(1), wght_top.unsqueeze(1), wght_bot.unsqueeze(1)), 1)
 
+        # remove graph filter
+        """
         # Perform the actural graph filtering: x = (I - \lambda L) * x
         wght_hori = wght_hori.unsqueeze(1).expand(-1, 3, -1, -1) # dimension expansion
         wght_vert = wght_vert.unsqueeze(1).expand(-1, 3, -1, -1)
@@ -53,7 +55,8 @@ class GraphFilter(nn.Module):
 
         pc_filt = pc + self.graph_lam * (pc_filt - torch.sum(wght_all,dim=1).unsqueeze(1).expand(-1, 3, -1, -1) * pc) # equivalent to ( I - \lambda L) * x
         pc_filt = pc_filt.view(bs_cur, 3, -1).permute([0, 2, 1])
-        return pc_filt, wght_all
+        """
+        return pc, wght_all
 
 
 class TearingNetGraphModel(nn.Module):
